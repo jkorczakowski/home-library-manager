b8817f0f5e853bc4b738ce41a770f0aaba736b70
jck <iuliuszkor@gmail.com>
2020-06-09 00:21:09 +0200

    Initial commit

---
+++ src/main/java/ovh/enyo/hlm/HomeLibraryManagerApplication.java
@@ -0,0 +1,13 @@
+package ovh.enyo.hlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLibraryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLibraryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ovh/enyo/hlm/HomeLibraryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.enyo.hlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLibraryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
